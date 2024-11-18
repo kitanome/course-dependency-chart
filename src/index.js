bee8665cca7c9bc0f5d53b51f63af1316c793aae
nguyenn <goalduc@gmail.com>
2024-11-17 20:29:26 -0500

    [feature/dagre-demo] graph now generates from json files (asych not implemented)

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
+let classList = await fetch('./sample.json')
+.then((res)=>{
+    if (!res.ok){
+        throw new Error(`HTTP Error`);
+    }
+    return res.json();
+})
+.then((data)=>{return data})
+.catch((error)=>{throw new Error("Error obtaining json file",error)});
+
 let g = new dagreD3.graphlib.Graph().setGraph({}).setDefaultEdgeLabel(function() {return {};});
 
-g.setNode("cs101", {label: "CS101", width: 100, height:100, style: "node"});
-g.setNode("cs201", {label: "CS201", width: 100, height:100, style: "node"});
-g.setNode("cs230", {label: "CS230", width: 100, height:100, style: "node"});
-g.setNode("cs326", {label: "CS236", width: 100, height:100, style: "node"});
-g.setNode("math210", {label: "MATH210", width: 100, height:100, style: "node"});
-g.setNode("cs410", {label: "CS410", width: 100, height:100, style: "node"});
-g.setNode("cs320", {label: "CS320", width: 100, height:100, style: "node"});
-g.setNode("cs240", {label: "CS240", width: 100, height:100, style: "node"});
-g.setNode("cs450", {label: "CS450", width: 100, height:100, style: "node"});
-g.setNode("cs370", {label: "CS370", width: 100, height:100, style: "node"});
+// g.setNode("cs101", {label: "CS101", width: 100, height:100, style: "node"});
+// g.setNode("cs201", {label: "CS201", width: 100, height:100, style: "node"});
+// g.setNode("cs230", {label: "CS230", width: 100, height:100, style: "node"});
+// g.setNode("cs326", {label: "CS236", width: 100, height:100, style: "node"});
+// g.setNode("math210", {label: "MATH210", width: 100, height:100, style: "node"});
+// g.setNode("cs410", {label: "CS410", width: 100, height:100, style: "node"});
+// g.setNode("cs320", {label: "CS320", width: 100, height:100, style: "node"});
+// g.setNode("cs240", {label: "CS240", width: 100, height:100, style: "node"});
+// g.setNode("cs450", {label: "CS450", width: 100, height:100, style: "node"});
+// g.setNode("cs370", {label: "CS370", width: 100, height:100, style: "node"});
 
-g.setEdge("cs101","cs201");
-g.setEdge("cs101","cs230");
-g.setEdge("cs201","cs326");
+// g.setEdge("cs101","cs201");
+// g.setEdge("cs101","cs230");
+// g.setEdge("cs201","cs326");
 
-g.setEdge("cs201","cs410");
-g.setEdge("math210","cs410");
+// g.setEdge("cs201","cs410");
+// g.setEdge("math210","cs410");
 
-g.setEdge("cs201","cs320");
-g.setEdge("cs230","cs320");
+// g.setEdge("cs201","cs320");
+// g.setEdge("cs230","cs320");
 
-g.setEdge("cs201","cs240");
+// g.setEdge("cs201","cs240");
 
-g.setEdge("cs410","cs450");
-g.setEdge("math210","cs450");
+// g.setEdge("cs410","cs450");
+// g.setEdge("math210","cs450");
 
-g.setEdge("cs230","cs370");
+// g.setEdge("cs230","cs370");
+
+classList.forEach((e)=>
+    {   
+        g.setNode(e.course_id,{label:e.name,height:80,width:200,style:"node"});
+    }
+);
+
+classList.forEach((e)=>
+    {   
+        if (e.prerequisites.length > 0) e.prerequisites.forEach((classes)=>
+        {
+            g.setEdge(classes,e.course_id);
+        })
+    }
+);
+
+g.nodes.forEach()
 
 let render = new dagreD3.render();
 let svg = d3.select("svg"),svgGroup = svg.append("g");

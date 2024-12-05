f64adb605326994a9a061d6e8538a54f51603fa6
nguyenn <goalduc@gmail.com>
2024-12-04 21:12:17 -0500

    [m4/restructure] fixed MIME error

--- front-end/src/components/GraphComponent/GraphComponent.js
+++ front-end/src/components/GraphComponent/GraphComponent.js
@@ -1,4 +1,4 @@
-import { BaseComponent } from "../BaseComponent/BaseComponent";
+import { BaseComponent } from "../BaseComponent/BaseComponent.js";
 export class GraphComponent extends BaseComponent{
     #container = null;
     #graph = null;

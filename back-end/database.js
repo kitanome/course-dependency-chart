d4516bca8af10604b1b3ee6bfc2170d4356aaf8c
SecureDAO <93850661+SecureDAO@users.noreply.github.com>
2024-12-06 18:14:55 -0500

    Restructured Backend

--- back-end/database.js
+++ back-end/database.js
@@ -1,8 +1,19 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
+	dialect: "sqlite",
+	storage: "database.sqlite",
 });
+
+async function testConnection() {
+	try {
+		await sequelize.authenticate();
+		console.log("Database connection established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
+}
+
+testConnection();
 
 export default sequelize;

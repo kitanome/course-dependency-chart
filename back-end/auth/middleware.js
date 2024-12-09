95b3dc212f1569c0ac500a6edfe5fe9332ece3c1
nguyenn <goalduc@gmail.com>
2024-12-09 18:28:17 -0500

    [login/passportjs] setup auth check route to check login

---
+++ back-end/auth/middleware.js
@@ -0,0 +1,7 @@
+const factoryResponse = (status, message) => ({ status, message });
+
+export const isAuthenticated = (req, res, next) => {
+    return req.isAuthenticated()
+        ? next()
+        : res.status(401).json(factoryResponse(401, "Unauthorized"));
+}

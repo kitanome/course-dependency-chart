a2867a9b208693818d079520a7ba8ba11249c802
nguyenn <goalduc@gmail.com>
2024-12-09 17:51:11 -0500

    [login/passportjs] setup passportjs, adjusted login, logout, register routes. Setup session for passportjs

--- back-end/database.js
+++ back-end/database.js
@@ -1,8 +1,19 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize({
+export const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite'
 });
 
-export default sequelize;
+// const SequelStore = SequelizeStore(session.Store);
+// const db = new Sequelize({
+// 	database:'session',
+// 	dialect:'sqlite',
+// 	storage:'session.sqlite',
+// });
+
+// export const sessionStore = new SequelStore({
+// 	db: db,
+// 	checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+// 	expiration: 24 * 60 * 60 * 1000  // The maximum age (in milliseconds) of a valid session.
+// })

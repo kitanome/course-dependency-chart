59eeb97adec74fb90d0c9f9d9c463c3e98313e9f
nguyenn <goalduc@gmail.com>
2024-12-09 19:23:47 -0500

    [login/passportjs] add check for session in LoginComponent

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,17 +1,13 @@
 import express from "express";
-import { login, createUser } from "../controllers/userController.js";
+import { login, createUser, getProfile } from "../controllers/userController.js";
 import passport from "../auth/passport.js";
+import {isAuthenticated} from "../auth/middleware.js";
 
 const router = express.Router();
 
 // Login route
-router.post("/login", login);
-// router.post("/login/auth",
-//     passport.authenticate('local',{
-//         successRedirect:'/',
-//         failureRedirect:'/login',
-//     })
-// )
+router.post("/login", renderLogin);
+router.post("/login/auth", login);
 
 // Create new user
 router.post("/users", createUser);

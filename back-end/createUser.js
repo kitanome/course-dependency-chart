e5a041e342980498c238fde4a755ea479c8cfde2
Kiet Nguyen (Ken) <kiet.swe@gmail.com>
2024-12-10 15:39:16 -0500

    changed UserModel to include savedCourses

--- back-end/createUser.js
+++ back-end/createUser.js
@@ -11,6 +11,7 @@ async function createUser() {
   const user = await User.create({
     username: "test",
     password: await bcrypt.hash("123", 10),
+    savedCourses : JSON.stringify([]),
   });
   console.log("User created:", user.toJSON());
 }

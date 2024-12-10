e2799a94952b4b45dbd6be2a2bd8d26ab2d339e3
rolandpil <rolandpilenyi@gmail.com>
2024-12-10 18:19:48 -0500

    Link checkbox to event listener

--- front-end/src/components/SaveCourseComponent/SaveCourseComponent.js
+++ front-end/src/components/SaveCourseComponent/SaveCourseComponent.js
@@ -4,12 +4,13 @@ import { EventHub } from "../../eventhub/EventHub.js";
 export class SaveCourseComponent extends BaseComponent {
   #container = null;
   #hub = null;
+  #sanitizedId = null;
   constructor(courseId) {
     super();
     this.courseId = courseId;
     this.#hub = EventHub.getInstance();
     //replace space with underscore
-    this.sanitizedId = this.courseId.replace(/\s/g, "_");
+    this.#sanitizedId = this.courseId.replace(/\s/g, "_");
   }
 
   #createContainer() {
@@ -23,18 +24,33 @@ export class SaveCourseComponent extends BaseComponent {
     // Returns the HTML template for the component
     // Add: onchange="onCheckboxChange(this)"
     return `
-    <input type="checkbox" id="save-checkbox" name="save-checkbox" value="saved">
-    <label for="vehicle1">Save Course</label><br>
+    <input type="checkbox" id="save-checkbox"">
+    <label for="checkbox">Save Course</label><br>
     `;
+  }
+  #attachEventListeners() {
+    const checkbox = this.#container.querySelector("#save-checkbox");
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        console.log(this.#sanitizedId);
+        this.#publishNewTask("saveCourse", this.#sanitizedId);
+      } else {
+        this.#publishNewTask("removeCourse", this.#sanitizedId);
+      }
+    });
   }
 
   render() {
     if (this.#container) {
       return this.#container;
     }
-    // Creates container and event listeners for filter
+    // Creates container and event listeners for course component
     this.#createContainer();
-    //this.#attachEventListeners();
+    this.#attachEventListeners();
     return this.#container;
+  }
+
+  #publishNewTask(task, termInput) {
+    this.#hub.publish(task, termInput);
   }
 }
